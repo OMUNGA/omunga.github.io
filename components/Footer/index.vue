68c383fa36b342339b2591b14613f936927f9d96
Miguel Domingos <mighueldomingos@gmail.com>
2024-04-11 14:39:57 +0200

    feat(component): ✨  create footer component

---
+++ components/Footer/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <footer class="flex flex-col items-start gap-2 p-4 max-w-80">
+    <div>
+      <Logo />
+    </div>
+    <span class="dark:text-white text-slate-900 text-wrap text-sm"
+      >É projecto criado com o principal objectivo de incentivar a partilha de
+      conteúdo por parte dos profissionais de TI de Angola</span
+    >
+    <div>
+      <SocialMedia />
+    </div>
+  </footer>
+</template>
+
+<script setup lang="ts">
+import { SocialMedia, Logo } from "@/components";
+</script>
