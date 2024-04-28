c81d14acdeb1a3110fdbcb970779b8779263ac5e
Miguel Domingos <79005738+Miguel-Domingos@users.noreply.github.com>
2024-04-28 23:06:48 +0200

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   app: {
-    // baseURL: "/omunga-app/",
+    baseURL: "/omunga/",
     // buildAssetsDir: "assets",
   },
   devtools: { enabled: true },
